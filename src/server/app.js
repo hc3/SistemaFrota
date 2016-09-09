b47c4829d9720aabdcaac68aa3f23b5c5bb9767e
Santos Eliel <eliel.floyd@bol.com.br>
2016-09-09 15:04:09 -0300

    index funfando

--- src/server/app.js
+++ src/server/app.js
@@ -5,14 +5,20 @@ import datasource from './config/datasource';
 import driversRouter from './routes/drivers';
 import vehiclesRouter from './routes/vehicles';
 import tiresRouter from './routes/tires';
+import path from 'path';
 
 const app = express();
 app.config = config;
 app.datasource = datasource(app);
 
 app.set('port',7000);
-app.use(express.static(__dirname + '../../src/client'));
+app.use(express.static(path.join(__dirname , '../client/')));
 app.use(bodyParser.json());
+//app.use(express.logger());
+
+app.get('/',function(req,res) {
+  res.sendFile(path.join(__dirname , '../client/index.html'));
+});
 
 driversRouter(app);
 vehiclesRouter(app);
