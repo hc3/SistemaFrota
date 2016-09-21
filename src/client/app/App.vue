df415efcda13fac006fd06d373b3f18aea014cb6
hc3 <eliel.floyd@bol.com.br>
2016-09-20 22:56:20 -0300

    finalizado

--- src/client/app/App.vue
+++ src/client/app/App.vue
@@ -1,9 +1,11 @@
 <script>
 import AppMenu from './components/Menu.vue';
+import AppFooter from './components/Footer.vue';
 
 export default {
   components:{
     AppMenu,
+    AppFooter,
   },
   data () {
     return {
@@ -16,9 +18,13 @@ export default {
 
 <template>
   <div id="app">
+
     <app-menu></app-menu>
+
     <h1>{{ msg }}</h1>
     <input type="text" v-model="msg">
+
+    <app-footer></app-footer>
   </div>
 </template>
 
---
+++ src/client/app/components/Footer.vue
@@ -0,0 +1,40 @@
+<template>
+
+    <footer class="page-footer">
+        <div class="container">
+        <div class="row">
+            <div class="col l6 s12">
+            <h5 class="white-text">Sistema de Frota</h5>
+            <p class="grey-text text-lighten-4">Colocar algum texto legal</p>
+            </div>
+            <div class="col l4 offset-l2 s12">
+            <h5 class="white-text">Contatos</h5>
+            <ul>
+                <li><a class="grey-text text-lighten-3" href="#!">Link 1</a></li>
+                <li><a class="grey-text text-lighten-3" href="#!">Link 2</a></li>
+                <li><a class="grey-text text-lighten-3" href="#!">Link 3</a></li>
+                <li><a class="grey-text text-lighten-3" href="#!">Link 4</a></li>
+            </ul>
+            </div>
+        </div>
+        </div>
+        <div class="footer-copyright">
+        <div class="container">
+        © 2014 Copyright Text
+        <a class="grey-text text-lighten-4 right" href="#!">texto</a>
+        </div>
+        </div>
+    </footer>
+            
+</tempalte>
+
+<script> 
+    export default {
+
+    } 
+    
+</script>
+
+<style>
+
+</style>
