ea6b26fdd21a8ff7699d9ca6ae7f097774d80744
Santos Eliel <eliel.floyd@bol.com.br>
2016-10-03 08:26:01 -0300

    iniciando com o uirouter

---
+++ src/client/app/driver/driver.router.js
@@ -0,0 +1,39 @@
+(function() {
+
+  angular
+    .module('app')
+    .config(config);
+
+    function config($stateProvider) {
+      $stateProvider
+      
+        .state('listDriver', {
+            url:'/drivers',
+            templateUrl:'app/driver/templates/driver_list.html',
+            controller:'DriverController',
+            controllerAs:'DriverController'
+        })
+
+        .state('newDriver', {
+          url:'/drivers/new',
+          templateUrl:'app/driver/templates/driver_new.html',
+          controller:'DriverController',
+          controllerAs:'DriverController'
+        })
+
+        .state('editDriver', {
+          url:'/drivers/:id/edit',
+          templateUrl:'app/driver/templates/driver_update.html',
+          controller:'DriverController',
+          controllerAs:'DriverController'
+        })
+
+        .state('viewDriver', {
+          url:'/drivers/:id/view',
+          templateUrl:'app/driver/templates/driver_view.html',
+          controller:'DriverController',
+          controllerAs:'DriverController'
+        })
+    }
+
+})();
