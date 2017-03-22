e0e27b93c34ef7508dd7360904f2a1666e21842e
hc3 <eliel.floyd@bol.com.br>
2017-03-21 22:44:22 -0300

    enviando

--- src/server/config/config.js
+++ src/server/config/config.js
@@ -1,10 +1,9 @@
 export default {
-  database: 'bancoRN',
-  username: '',
-  password: '',
+  database: 'ddi7l328r0h09a',
+  username: 'dsiuaiapgodglq',
+  password: '7178c2797b9378dcb0dc3bca088b8a53308fa142052a8b11ea4c2b4307196e66',
     params: {
-      dialect: 'sqlite',
-      storage: 'bancoRN.sqlite',
+      dialect: 'postgres',
     define: {
       underscored: true
     },
