8c07dfe6af91d8f3060ddec073f2dedea6196461
hc3 <eliel.floyd@bol.com.br>
2017-04-09 15:25:35 -0300

    atualizando teste de integração de tire

--- src/server/test/integration/routes/tires.js
+++ src/server/test/integration/routes/tires.js
@@ -1,4 +1,5 @@
 import jwt from 'jwt-simple';
+import callback from './callback-integration';
 
 describe('# TEST INTEGRATION # Routes Tires', () => {
 
@@ -10,36 +11,42 @@ describe('# TEST INTEGRATION # Routes Tires', () => {
   let token;
 
   const defaultDriver = {
-    id:1,
-    cod:800,
-    name:'ASTROGILDO',
-    phone:91158875
+    id: 1,
+    cod: 800,
+    name: 'ASTROGILDO',
+    phone: 91158875
   }
 
   const defaultVehicle = {
-    id:1,
-    placa:'TXT-8890',
-    modelo:'FORD CARGO 1723',
-    marca:'FORD',
-    eixos:4,
-    km_inicial:0,
-    driver_id:defaultDriver.id
+    id: 1,
+    placa: 'TXT-8890',
+    modelo: 'FORD CARGO 1723',
+    marca: 'FORD',
+    eixos: 4,
+    km_inicial: 0,
+    driver_id: defaultDriver.id
   };
 
   const defaultTire = {
-    id:1,
-    cod:10,
-    marca:'PIRELLI',
-    vida:1,
-    sulco:1,
-    recap:false,
-    trash:false,
-    vehicle_id:defaultVehicle.id
+    id: 1,
+    cod: 10,
+    marca: 'PIRELLI',
+    vida: 1,
+    sulco: 1,
+    recap: false,
+    trash: false,
+    vehicle_id: defaultVehicle.id
   };
+
+  const ROTA = '/tires';
+  const ROTA_ID = '/tires/1';
+  const ROTA_BY_COD = '/tiresByCodigo/10';
 
   beforeEach(done => {
     Users
-      .destroy({where: {}})
+      .destroy({
+        where: {}
+      })
       .then(() => Users.create({
         name: 'pepa',
         email: 'pepa@mail.com',
@@ -47,18 +54,26 @@ describe('# TEST INTEGRATION # Routes Tires', () => {
       }))
       .then(user => {
         Drivers
-          .destroy({where:{}})
+          .destroy({
+            where: {}
+          })
           .then(() => Drivers.create(defaultDriver))
 
         Vehicles
-          .destroy({where:{}})
+          .destroy({
+            where: {}
+          })
           .then(() => Vehicles.create(defaultVehicle))
 
         Tires
-          .destroy({where:{}})
+          .destroy({
+            where: {}
+          })
           .then(() => Tires.create(defaultTire))
           .then(() => {
-            token = jwt.encode({id: user.id}, jwtSecret);
+            token = jwt.encode({
+              id: user.id
+            }, jwtSecret);
             done();
           })
       })
@@ -66,125 +81,47 @@ describe('# TEST INTEGRATION # Routes Tires', () => {
   });
 
   describe('Route get /tires', () => {
-    it('should return a list of tires', done => {
-      request
-        .get('/tires')
-        .set('Authorization', `JWT ${token}`)
-        .end((err,res) => {
-          expect(res.body[0].id).to.be.eql(defaultTire.id);
-          expect(res.body[0].cod).to.be.eql(defaultTire.cod);
-          expect(res.body[0].marca).to.be.eql(defaultTire.marca);
-          expect(res.body[0].vida).to.be.eql(defaultTire.vida);
-          expect(res.body[0].sulco).to.be.eql(defaultTire.sulco);
-          expect(res.body[0].recap).to.be.eql(defaultTire.recap);
-          expect(res.body[0].trash).to.be.eql(defaultTire.trash);
-          expect(res.body[0].vehicle_id).to.be.eql(defaultTire.vehicle_id);
-          done(err);
-        });
-    });
+    it('should return a list of tires', done => callback.defaultGet(done, request, token, defaultTire, ROTA))
   });
 
-  describe('Route PUT /tires', () => {
-    it('should create a tires', done => {
-      const createdTire = {
-        id:2,
-        cod:11,
-        marca:'CREATED TIRE',
-        vida:2,
-        sulco:2,
-        recap:false,
-        trash:false,
-        vehicle_id:defaultVehicle.id
+  describe('Route POST /tires', () => {
+    const createdTire = {
+        id: 2,
+        cod: 11,
+        marca: 'CREATED TIRE',
+        vida: 2,
+        sulco: 2,
+        recap: false,
+        trash: false,
+        vehicle_id: defaultVehicle.id
       };
-      request
-        .post('/tires')
-        .set('Authorization', `JWT ${token}`)
-        .send(createdTire)
-        .end((err,res) => {
-          expect(res.body.id).to.be.eql(createdTire.id);
-          expect(res.body.cod).to.be.eql(createdTire.cod);
-          expect(res.body.marca).to.be.eql(createdTire.marca);
-          expect(res.body.vida).to.be.eql(createdTire.vida);
-          expect(res.body.sulco).to.be.eql(createdTire.sulco);
-          expect(res.body.recap).to.be.eql(createdTire.recap);
-          expect(res.body.trash).to.be.eql(createdTire.trash);
-          expect(res.body.vehicle_id).to.be.eql(createdTire.vehicle_id);
-          done(err);
-        });
-    });
+    it('should create a tires', done => callback.defaultPost(done,request,token,createdTire,ROTA))
   });
 
   describe('Route GET /tires/:id', () => {
-    it('should find a one tires', done => {
-      request
-        .get('/tires/1')
-        .set('Authorization', `JWT ${token}`)
-        .end((err,res) => {
-          expect(res.body.id).to.be.eql(defaultTire.id);
-          expect(res.body.cod).to.be.eql(defaultTire.cod);
-          expect(res.body.marca).to.be.eql(defaultTire.marca);
-          expect(res.body.vida).to.be.eql(defaultTire.vida);
-          expect(res.body.sulco).to.be.eql(defaultTire.sulco);
-          expect(res.body.recap).to.be.eql(defaultTire.recap);
-          expect(res.body.trash).to.be.eql(defaultTire.trash);
-          expect(res.body.vehicle_id).to.be.eql(defaultTire.vehicle_id);
-          done(err);
-        });
-    });
+    it('should find a one tires', done => callback.defaultGetOne(done, request, token, defaultTire, ROTA_ID))
   });
 
   describe('Route GET /tires/:cod', () => {
-    it('should find a one tires', done => {
-      request
-        .get('/tiresByCodigo/10')
-        .set('Authorization', `JWT ${token}`)
-        .end((err,res) => {
-          expect(res.body[0].id).to.be.eql(defaultTire.id);
-          expect(res.body[0].cod).to.be.eql(defaultTire.cod);
-          expect(res.body[0].marca).to.be.eql(defaultTire.marca);
-          expect(res.body[0].vida).to.be.eql(defaultTire.vida);
-          expect(res.body[0].sulco).to.be.eql(defaultTire.sulco);
-          expect(res.body[0].recap).to.be.eql(defaultTire.recap);
-          expect(res.body[0].trash).to.be.eql(defaultTire.trash);
-          expect(res.body[0].vehicle_id).to.be.eql(defaultTire.vehicle_id);
-          done(err);
-        });
-    });
+    it('should find a one tires', done => callback.defaultGet(done, request, token, defaultTire, ROTA_BY_COD))
   });
 
   describe('Route PUT /tires/:id', () => {
-    it('should update a tires', done => {
       const updatedTire = {
-        id:2,
-        cod:11,
-        marca:'UPDATED TIRE',
-        vida:2,
-        sulco:2,
-        recap:false,
-        trash:false,
-        vehicle_id:defaultVehicle.id
+        id: 2,
+        cod: 11,
+        marca: 'UPDATED TIRE',
+        vida: 2,
+        sulco: 2,
+        recap: false,
+        trash: false,
+        vehicle_id: defaultVehicle.id
       };
-      request
-        .put('/tires/1')
-        .set('Authorization', `JWT ${token}`)
-        .send(updatedTire)
-        .end((err,res) => {
-          expect(res.body).to.be.eql([1])
-          done(err);
-        });
-    });
+    it('should update a tires', done => callback.defaultPut(done, request, token, updatedTire, ROTA_ID));
   });
 
   describe('Route DELETE /tires/:id', () => {
-    it('should delete a tires', done => {
-      request
-        .delete('/tires/1')
-        .set('Authorization', `JWT ${token}`)
-        .end((err,res) => {
-          expect(res.statusCode).to.be.eql(204);
-          done(err);
-        });
-    });
+    it('should delete a tires', done => callback.defaultGetOne(done, request, token, defaultTire, ROTA_ID))
   });
 
 })
